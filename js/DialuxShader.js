f307d7d2749e60e4bdfdbe9629180a72b2083d9d
viton930 <viton940@gmail.com>
2023-07-21 23:53:56 +0300

    finish first

---
+++ js/DialuxShader.js
@@ -0,0 +1,484 @@
+var DialuxShader = {
+
+	uniforms: {
+
+		// params ----------------------------------------------------------------
+		"diffuse": { type: "c", value: new THREE.Color(0xff0000) },
+		"ambient": { type: "c", value: new THREE.Color(0x000000) },
+		"specular": { type: "c", value: new THREE.Color(0x111111) },
+		"shininess": { type: "f", value: 30 },
+		"refractionRatio": { type: "f", value: 1.00 },
+		"opacity": { type: "f", value: 1.0 },
+		"envMap": { type: "t", value: null },
+
+
+		// lights ----------------------------------------------------------------
+		"ambientLightColor": { type: "fv", value: [] },
+
+		"directionalLightDirection": { type: "fv", value: [] },
+		"directionalLightColor": { type: "fv", value: [] },
+
+		"hemisphereLightDirection": { type: "fv", value: [] },
+		"hemisphereLightSkyColor": { type: "fv", value: [] },
+		"hemisphereLightGroundColor": { type: "fv", value: [] },
+
+		"pointLightColor": { type: "fv", value: [] },
+		"pointLightPosition": { type: "fv", value: [] },
+		"pointLightDistance": { type: "fv1", value: [] },
+
+		"spotLightColor": { type: "fv", value: [] },
+		"spotLightPosition": { type: "fv", value: [] },
+		"spotLightDirection": { type: "fv", value: [] },
+		"spotLightDistance": { type: "fv1", value: [] },
+		"spotLightAngleCos": { type: "fv1", value: [] },
+		"spotLightExponent": { type: "fv1", value: [] },
+
+
+		// not use
+		"emissive": { type: "c", value: new THREE.Color(0xff0000) },
+		"reflectivity": { type: "f", value: 1.0 },
+		"map": { type: "t", value: null },
+		"offsetRepeat": { type: "v4", value: new THREE.Vector4(0, 0, 1, 1) },
+		"lightMap": { type: "t", value: null },
+		"specularMap": { type: "t", value: null },
+		"flipEnvMap": { type: "f", value: -1 },
+		"useRefract": { type: "i", value: 0 },
+		"combine": { type: "i", value: 0 },
+
+	}
+
+
+
+	,
+
+	vertexShader: [
+
+		"uniform float refractionRatio;",
+
+		"varying vec3 vViewPosition;",
+		"varying vec3 vNormal;",
+		"varying vec3 vReflect;",
+		"varying vec3 vRefract;",
+
+
+		// lights_phong_pars_vertex ----------------------------------------
+
+		"#ifndef PHONG_PER_PIXEL",
+
+		"#if MAX_POINT_LIGHTS > 0",
+
+		"uniform vec3 pointLightPosition[ MAX_POINT_LIGHTS ];",
+		"uniform float pointLightDistance[ MAX_POINT_LIGHTS ];",
+
+		"varying vec4 vPointLight[ MAX_POINT_LIGHTS ];",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0",
+
+		"uniform vec3 spotLightPosition[ MAX_SPOT_LIGHTS ];",
+		"uniform float spotLightDistance[ MAX_SPOT_LIGHTS ];",
+
+		"varying vec4 vSpotLight[ MAX_SPOT_LIGHTS ];",
+
+		"#endif",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0 || defined( USE_BUMPMAP )",
+
+		"varying vec3 vWorldPosition;",
+
+		"#endif",
+		// ----------------------------------------------------------------
+
+
+		"void main() {",
+
+
+		"vec3 objectNormal = normal;",
+		"vec3 transformedNormal = normalMatrix * objectNormal;",
+
+		"vNormal = normalize( transformedNormal );",
+
+		"vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
+		"gl_Position = projectionMatrix * mvPosition;",
+
+		"vViewPosition = -mvPosition.xyz;",
+
+		// envMap ----------------------------------------------------------------
+		"vec4 worldPosition = modelMatrix * vec4( position, 1.0 );",
+		"vec3 worldNormal = mat3( modelMatrix[ 0 ].xyz, modelMatrix[ 1 ].xyz, modelMatrix[ 2 ].xyz ) * objectNormal;",
+		"worldNormal = normalize( worldNormal );",
+
+		//	"float length = sqrt(pow(cameraToVertex.x,2.0)+pow(cameraToVertex.y,2.0)+pow(cameraToVertex.z,2.0));",
+		//	"float K = (length - 50.0)/length;",
+		"vec3 cameraToVertex = normalize( worldPosition.xyz - cameraPosition );",
+		"cameraToVertex = normalize(cameraToVertex);",
+		"vRefract = refract( cameraToVertex, worldNormal, 1.0/refractionRatio );",
+		"vReflect = reflect( cameraToVertex, worldNormal );",
+
+		// 	lights_phong_vertex ---------------------------------------------------------------
+
+		"#ifndef PHONG_PER_PIXEL",
+
+		"#if MAX_POINT_LIGHTS > 0",
+
+		"for( int i = 0; i < MAX_POINT_LIGHTS; i ++ ) {",
+
+		"vec4 lPosition = viewMatrix * vec4( pointLightPosition[ i ], 1.0 );",
+		"vec3 lVector = lPosition.xyz - mvPosition.xyz;",
+
+		"float lDistance = 1.0;",
+		"if ( pointLightDistance[ i ] > 0.0 )",
+		"lDistance = 1.0 - min( ( length( lVector ) / pointLightDistance[ i ] ), 1.0 );",
+
+		"vPointLight[ i ] = vec4( lVector, lDistance );",
+
+		"}",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0",
+
+		"for( int i = 0; i < MAX_SPOT_LIGHTS; i ++ ) {",
+
+		"vec4 lPosition = viewMatrix * vec4( spotLightPosition[ i ], 1.0 );",
+		"vec3 lVector = lPosition.xyz - mvPosition.xyz;",
+
+		"float lDistance = 1.0;",
+		"if ( spotLightDistance[ i ] > 0.0 )",
+		"lDistance = 1.0 - min( ( length( lVector ) / spotLightDistance[ i ] ), 1.0 );",
+
+		"vSpotLight[ i ] = vec4( lVector, lDistance );",
+
+		"}",
+
+		"#endif",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0 || defined( USE_BUMPMAP )",
+
+		"vWorldPosition = worldPosition.xyz;",
+
+		"#endif",
+
+		// ----------------------------------------------------------------
+
+
+		"}"
+
+	].join("\n"),
+
+	fragmentShader: [
+
+		"uniform vec3 diffuse;",
+		"uniform vec3 ambient;",
+		"uniform vec3 specular;",
+		"uniform float shininess;",
+		"uniform float opacity;",
+		"uniform samplerCube envMap;",
+
+		"varying vec3 vReflect;",
+		"varying vec3 vRefract;",
+
+		// lights_phong_pars_fragment ----------------------------------------------------------------
+
+		"uniform vec3 ambientLightColor;",
+
+		"#if MAX_DIR_LIGHTS > 0",
+
+		"uniform vec3 directionalLightColor[ MAX_DIR_LIGHTS ];",
+		"uniform vec3 directionalLightDirection[ MAX_DIR_LIGHTS ];",
+
+		"#endif",
+
+		"#if MAX_HEMI_LIGHTS > 0",
+
+		"uniform vec3 hemisphereLightSkyColor[ MAX_HEMI_LIGHTS ];",
+		"uniform vec3 hemisphereLightGroundColor[ MAX_HEMI_LIGHTS ];",
+		"uniform vec3 hemisphereLightDirection[ MAX_HEMI_LIGHTS ];",
+
+		"#endif",
+
+		"#if MAX_POINT_LIGHTS > 0",
+
+		"uniform vec3 pointLightColor[ MAX_POINT_LIGHTS ];",
+
+		"#ifdef PHONG_PER_PIXEL",
+
+		"uniform vec3 pointLightPosition[ MAX_POINT_LIGHTS ];",
+		"uniform float pointLightDistance[ MAX_POINT_LIGHTS ];",
+
+		"#else",
+
+		"varying vec4 vPointLight[ MAX_POINT_LIGHTS ];",
+
+		"#endif",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0",
+
+		"uniform vec3 spotLightColor[ MAX_SPOT_LIGHTS ];",
+		"uniform vec3 spotLightPosition[ MAX_SPOT_LIGHTS ];",
+		"uniform vec3 spotLightDirection[ MAX_SPOT_LIGHTS ];",
+		"uniform float spotLightAngleCos[ MAX_SPOT_LIGHTS ];",
+		"uniform float spotLightExponent[ MAX_SPOT_LIGHTS ];",
+
+		"#ifdef PHONG_PER_PIXEL",
+
+		"uniform float spotLightDistance[ MAX_SPOT_LIGHTS ];",
+
+		"#else",
+
+		"varying vec4 vSpotLight[ MAX_SPOT_LIGHTS ];",
+
+		"#endif",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0 || defined( USE_BUMPMAP )",
+
+		"varying vec3 vWorldPosition;",
+
+		"#endif",
+
+		"#ifdef WRAP_AROUND",
+
+		"uniform vec3 wrapRGB;",
+
+		"#endif",
+
+		"varying vec3 vViewPosition;",
+		"varying vec3 vNormal;",
+
+
+		"void main() {",
+
+		"gl_FragColor = vec4( vec3 ( 1.0 ), 1.0 );",
+		"float specularStrength = 1.0;",
+
+
+		//	Envmap mix ----------------------------------------------------
+		"vec4 reflectedColor = textureCube( envMap, vec3( - vReflect.x, vReflect.yz ) );",
+		"vec4 refractedColor = textureCube( envMap, vec3(  - vRefract.x, vRefract.yz ) );",
+		"vec4 cubeColor = mix( reflectedColor, refractedColor , clamp( opacity, 0.0, 1.0 ) );",
+
+		// lights_phong_fragment  ----------------------------------------------------
+		"vec3 normal = normalize( vNormal );",
+		"vec3 viewPosition = normalize( vViewPosition );",
+
+
+		"#if MAX_POINT_LIGHTS > 0",
+
+		"vec3 pointDiffuse  = vec3( 0.0 );",
+		"vec3 pointSpecular = vec3( 0.0 );",
+
+		"for ( int i = 0; i < MAX_POINT_LIGHTS; i ++ ) {",
+
+		"vec4 lPosition = viewMatrix * vec4( pointLightPosition[ i ], 1.0 );",
+		"vec3 lVector = lPosition.xyz + vViewPosition.xyz;",
+
+		"float lDistance = 1.0;",
+		"if ( pointLightDistance[ i ] > 0.0 )",
+		"lDistance = 1.0 - min( ( length( lVector ) / pointLightDistance[ i ] ), 1.0 );",
+
+		"lVector = normalize( lVector );",
+
+		// diffuse
+
+		"float dotProduct = dot( normal, lVector );",
+
+		"float pointDiffuseWeight = max( dotProduct, 0.0 );",
+
+		"pointDiffuse  += diffuse * pointLightColor[ i ] * pointDiffuseWeight * lDistance;",
+
+		// specular
+
+		"vec3 pointHalfVector = normalize( lVector + viewPosition );",
+		"float pointDotNormalHalf = max( dot( normal, pointHalfVector ), 0.0 );",
+		"float pointSpecularWeight = specularStrength * max( pow( pointDotNormalHalf, shininess ), 0.0 );",
+
+		"pointSpecular += specular * pointLightColor[ i ] * pointSpecularWeight * pointDiffuseWeight * lDistance;",
+
+		"}",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0",
+
+		"vec3 spotDiffuse  = vec3( 0.0 );",
+		"vec3 spotSpecular = vec3( 0.0 );",
+
+		"for ( int i = 0; i < MAX_SPOT_LIGHTS; i ++ ) {",
+
+		"vec4 lPosition = viewMatrix * vec4( spotLightPosition[ i ], 1.0 );",
+		"vec3 lVector = lPosition.xyz + vViewPosition.xyz;",
+
+		"float lDistance = 1.0;",
+		"if ( spotLightDistance[ i ] > 0.0 )",
+		"lDistance = 1.0 - min( ( length( lVector ) / spotLightDistance[ i ] ), 1.0 );",
+
+		"lVector = normalize( lVector );",
+
+		"float spotEffect = dot( spotLightDirection[ i ], normalize( spotLightPosition[ i ] - vWorldPosition ) );",
+
+		"if ( spotEffect > spotLightAngleCos[ i ] ) {",
+
+		"spotEffect = max( pow( spotEffect, spotLightExponent[ i ] ), 0.0 );",
+
+		// diffuse
+
+		"float dotProduct = dot( normal, lVector );",
+
+		"float spotDiffuseWeight = max( dotProduct, 0.0 );",
+		"spotDiffuse += diffuse * spotLightColor[ i ] * spotDiffuseWeight * lDistance * spotEffect;",
+
+		// specular
+
+		"vec3 spotHalfVector = normalize( lVector + viewPosition );",
+		"float spotDotNormalHalf = max( dot( normal, spotHalfVector ), 0.0 );",
+		"float spotSpecularWeight = specularStrength * max( pow( spotDotNormalHalf, shininess ), 0.0 );",
+
+		"spotSpecular += specular * spotLightColor[ i ] * spotSpecularWeight * spotDiffuseWeight * lDistance * spotEffect;",
+
+
+		"}",
+
+		"}",
+
+		"#endif",
+
+		"#if MAX_DIR_LIGHTS > 0",
+
+		"vec3 dirDiffuse  = vec3( 0.0 );",
+		"vec3 dirSpecular = vec3( 0.0 );",
+
+		"for( int i = 0; i < MAX_DIR_LIGHTS; i ++ ) {",
+
+		"vec4 lDirection = viewMatrix * vec4( directionalLightDirection[ i ], 0.0 );",
+		"vec3 dirVector = normalize( lDirection.xyz );",
+
+		// diffuse
+
+		"float dotProduct = dot( normal, dirVector );",
+
+
+		"float dirDiffuseWeight = max( dotProduct, 0.0 );",
+
+		// "#endif",
+
+		"dirDiffuse  += diffuse * directionalLightColor[ i ] * dirDiffuseWeight;",
+
+		// specular
+
+		"vec3 dirHalfVector = normalize( dirVector + viewPosition );",
+		"float dirDotNormalHalf = max( dot( normal, dirHalfVector ), 0.0 );",
+		"float dirSpecularWeight = specularStrength * max( pow( dirDotNormalHalf, shininess ), 0.0 );",
+
+
+		"dirSpecular += specular * directionalLightColor[ i ] * dirSpecularWeight * dirDiffuseWeight;",
+
+		"}",
+
+		"#endif",
+
+		"#if MAX_HEMI_LIGHTS > 0",
+
+		"vec3 hemiDiffuse  = vec3( 0.0 );",
+		"vec3 hemiSpecular = vec3( 0.0 );",
+
+		"for( int i = 0; i < MAX_HEMI_LIGHTS; i ++ ) {",
+
+		"vec4 lDirection = viewMatrix * vec4( hemisphereLightDirection[ i ], 0.0 );",
+		"vec3 lVector = normalize( lDirection.xyz );",
+
+		// diffuse
+
+		"float dotProduct = dot( normal, lVector );",
+		"float hemiDiffuseWeight = 0.5 * dotProduct + 0.5;",
+
+		"vec3 hemiColor = mix( hemisphereLightGroundColor[ i ], hemisphereLightSkyColor[ i ], hemiDiffuseWeight );",
+
+		"hemiDiffuse += diffuse * hemiColor;",
+
+		// specular (sky light)
+
+		"vec3 hemiHalfVectorSky = normalize( lVector + viewPosition );",
+		"float hemiDotNormalHalfSky = 0.5 * dot( normal, hemiHalfVectorSky ) + 0.5;",
+		"float hemiSpecularWeightSky = specularStrength * max( pow( hemiDotNormalHalfSky, shininess ), 0.0 );",
+
+		// specular (ground light)
+
+		"vec3 lVectorGround = -lVector;",
+
+		"vec3 hemiHalfVectorGround = normalize( lVectorGround + viewPosition );",
+		"float hemiDotNormalHalfGround = 0.5 * dot( normal, hemiHalfVectorGround ) + 0.5;",
+		"float hemiSpecularWeightGround = specularStrength * max( pow( hemiDotNormalHalfGround, shininess ), 0.0 );",
+
+		"hemiSpecular += specular * hemiColor * ( hemiSpecularWeightSky + hemiSpecularWeightGround ) * hemiDiffuseWeight;",
+
+		"}",
+
+		"#endif",
+
+		"vec3 totalDiffuse = vec3( 0.0 );",
+		"vec3 totalSpecular = vec3( 0.0 );",
+
+		"#if MAX_DIR_LIGHTS > 0",
+
+		"totalDiffuse += dirDiffuse;",
+		"totalSpecular += dirSpecular;",
+
+		"#endif",
+
+		"#if MAX_HEMI_LIGHTS > 0",
+
+		"totalDiffuse += hemiDiffuse;",
+		"totalSpecular += hemiSpecular;",
+
+		"#endif",
+
+		"#if MAX_POINT_LIGHTS > 0",
+
+		"totalDiffuse += pointDiffuse;",
+		"totalSpecular += pointSpecular;",
+
+		"#endif",
+
+		"#if MAX_SPOT_LIGHTS > 0",
+
+		"totalDiffuse += spotDiffuse;",
+		"totalSpecular += spotSpecular;",
+
+		"#endif",
+
+
+		// SUM ------------------------------------------------------------------
+
+		"gl_FragColor.xyz = cubeColor.xyz * ambient.xyz;",
+		"gl_FragColor.xyz += totalDiffuse.xyz ;",
+		"gl_FragColor.xyz += totalSpecular.xyz ;",
+
+		"}"
+
+	].join("\n")
+
+}
+
+class DialuxMaterial extends THREE.MeshPhongMaterial {
+
+	constructor(parameters) {
+		super(parameters);
+
+		this.uniforms = DialuxShader.uniforms;
+		this.vertexShader = DialuxShader.vertexShader;
+		this.fragmentShader = DialuxShader.fragmentShader;
+	}
+
+}
+
+// export { DialuxMaterial }
+
---
+++ js/MaterialMaster.js
@@ -0,0 +1,499 @@
+
+import { GUI } from './libs/lil-gui.module.min.js';
+
+/* Лакированный материал
+----------------------------------------------------------------
+    Система уравнений:
+    Yspec = mProp.Kspec_refl * mProp.Yrefl = K
+    Ydiff = (1 - Yspec) * Y
+    Yrefl = Yspec + Ydiff
+    Решение:
+    Yrefl = K + ( 1 - K ) * Y
+    Kspec_refl = K / Yrefl
+*/
+
+/* Прозрачный материал
+----------------------------------------------------------------
+    Система уравнений:
+    Ytrans/Yrefl = mProp.Ytrans/mProp.Yrefl = K
+    Yrefl + Ytrans = Y
+    Решение:
+    Yrefl = Y/(1+K)
+    Ytrans = K*Yrefl
+ */
+
+// Constants (https://en.wikipedia.org/wiki/Relative_luminance)
+const K_R = 0.2126;
+const K_G = 0.7152;
+const K_B = 0.0722;
+
+// sRGB <-> lRGB (https://mina86.com/2019/srgb-xyz-conversion) 
+
+function toLinear(value) {
+    if (value <= 0.04045) {
+        value = value / 12.92;
+    } else {
+        value = Math.pow((value + 0.055) / 1.055, 2.4);
+    }
+    return value;
+}
+
+function fromLinear(value) {
+    if (value <= 0.0031308) {
+        value = value * 12.92; // * 255
+    } else {
+        value = 1.055 * Math.pow(value, 1 / 2.4) - 0.055; //* 255
+    }
+    return value;
+}
+
+// Change rgb by Luminance ----------------------------------------------------------------
+
+function changeY(color, Ynew) {
+    const K = [K_R, K_G, K_B]
+
+    // foe no division to null errors
+    for (var j = 0; j < 3; j++) {
+        if (!color[j])
+            color[j] = 0.000000001;
+    }
+
+    var sum = K[0] * color[0] + K[1] * color[1] + K[2] * color[2];
+    var coeff = Ynew / sum;
+    var index = rgb_max_i(color);
+    color[index] *= coeff;
+    if (color[index] > 1) {
+        color[index] = 1;
+        sum = 0;
+        for (var j = 0; j < 3; j++) {
+            if (j == index) continue;
+            sum += color[j] * K[j]
+        }
+        coeff = (Ynew - K[index]) / sum;
+        var index2 = rgb_max_i(color, index);
+        color[index2] *= coeff;
+        if (color[index2] > 1) {
+            color[index2] = 1;
+            var index3;
+            for (var j = 0; j < 3; j++) {
+                if (j == index || j == index2) continue;
+                sum += color[j] * K[j]
+                index3 = j;
+            }
+            coeff = (Ynew - K[index] - K[index2]) / sum;
+
+            color[index3] *= coeff;
+
+            if (color[index3] > 1)
+                color[index3] = 1;
+
+        }
+        else {
+            for (var j = 0; j < 3; j++) {
+                if (j == index || j == index2) continue;
+                color[j] *= coeff;
+            }
+        }
+    } else {
+        for (var j = 0; j < 3; j++) {
+            if (j == index) continue;
+            color[j] *= coeff;
+        }
+    }
+
+    return color;
+}
+
+
+function changeLuminance(rgb, Ynew) {
+    var color = [...rgb];
+
+    // sRGB to linear RGB
+    color[0] = toLinear(color[0]);
+    color[1] = toLinear(color[1]);
+    color[2] = toLinear(color[2]);
+
+    // Change luminance
+    color = changeY(color, Ynew);
+
+    // linear RGB to sRGB
+    color[0] = fromLinear(color[0])
+    color[1] = fromLinear(color[1])
+    color[2] = fromLinear(color[2])
+
+    return color;
+}
+
+
+// Tools ----------------------------------------------------------------
+
+function check_value(val) {
+    if (val.upd === false) {
+        val.upd = true;
+        return false;
+    }
+    return true;
+}
+
+function elem_vis(elem, flag) {
+    var str = flag ? "" : "none";
+    elem.domElement.style.display = str;
+}
+
+function rgb_max_i(arr, no_index = -1) {
+    var max = 0;
+    var index = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i == no_index) continue;
+        if (arr[i] > max) {
+            max = arr[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+
+// MaterialMaster ----------------------------------------------------------------
+
+class MaterialMaster {
+
+    MType = class {
+        Metallic = "Metallic"
+        Painted = "Painted"
+        Transparent = "Transparent"
+
+        getArr() { return [this.Metallic, this.Painted, this.Transparent]; }
+    };
+
+    Color = [0, 0, 0];
+    Type = "";
+    Props = {}
+    material = {};
+    Koeff = 0;
+
+    constructor() {
+    }
+
+    init(m) {
+        this.material = m;
+        this.init_gui();
+        this.setType();
+    }
+
+    init_gui() {
+
+        var def_props = {
+            Color: [0, 0, 0],                 // Material Color
+            Type: "Metallic",                 // Material type
+            Shin: 40,                         // Shininess
+            Refl: 0.0,                        // Reflection factor        [0,0.9]
+            Kspec_refl: 0.0,                  // Reflective coating       [0,1]
+            Trans: 0.0,                       // Degree of transmission   [0,1]
+            n: 1.0                            // Refractive index         [1,2]
+        }
+
+        var Props = this.Props;
+        var gui = new GUI({ name: "Material" });
+
+        Props.Color = gui.addColor(def_props, 'Color').name('Color').listen();
+        Props.Color.onChange(value => this.setColor());
+
+        Props.Type = gui.add(def_props, 'Type', ["Metallic", "Painted", "Transparent"]).name('Material Type').listen();
+        Props.Type.onChange(value => this.setType());
+
+        Props.Refl = gui.add(def_props, 'Refl').min(0.00).max(0.9).step(0.01).name('Reflection factor').listen();
+        Props.Refl.onChange(value => this.setRefl());
+
+        Props.Kspec_refl = gui.add(def_props, 'Kspec_refl').min(0.00).max(1.00).step(0.01).name('Reflective coating').listen();
+        Props.Kspec_refl.onChange(value => this.setKspec_refl());
+
+        Props.Trans = gui.add(def_props, 'Trans').min(0.00).max(1.00).step(0.01).name('Degree of trans.').listen();
+        Props.Trans.onChange(value => this.setTrans());
+
+        Props.n = gui.add(def_props, 'n').min(1.00).max(2.00).step(0.01).name('Refractive index').listen();
+        Props.n.onChange(value => this.setN());
+    }
+
+    render() {
+        var Props = this.Props;
+        var type = Props.Type.getValue();
+        var Kspec_refl = Props.Kspec_refl.getValue();
+        var Refl = Props.Refl.getValue();
+        var Trans = Props.Trans.getValue();
+
+        var color = Props.Color.getValue();
+
+        var diff, spec, amb;
+        var Shin, N, opacity;
+        if (type == "Metallic") {
+
+            var Ys = Refl * Kspec_refl;
+            var Yd = Refl * (1 - Kspec_refl);
+            diff = changeLuminance(color, Yd);
+            spec = changeLuminance(color, Ys);
+            amb = spec;
+
+            opacity = 0;
+            Shin = 40; // примерно
+            N = 1;
+        }
+        else if (type == "Painted") {
+
+            var Ys = Refl * Kspec_refl;
+            var Yd = Refl * (1 - Ys);
+            diff = changeLuminance(color, Yd);
+            spec = changeLuminance([0, 0, 0], Ys); // grayscale
+            amb = spec;
+
+            opacity = 0;
+            Shin = 80; // примерно   (10 + 2 * Math.pow(Refl, 1.5) * 90);
+            N = 1;
+
+        }
+        else if (type == "Transparent") {
+            var Y = Refl + Trans;
+            diff = [0, 0, 0];
+            spec = changeLuminance(color, Refl); // grayscale
+            amb = changeLuminance(color, Y); // grayscale
+
+            opacity = Trans / Y;
+            Shin = 40; // примерно
+            N = Props.n.getValue();
+        }
+
+
+        this.SetMaterial(diff, spec, amb, opacity, Shin, N);
+    }
+
+    // GetY ----------------------------------------------------------------
+    getYfromRgb() {
+        var color = this.Props.Color.getValue();//this.Color;
+        // sRGB to Y
+        var Yrgb = [0, 0, 0];
+        Yrgb[0] = toLinear(color[0]);
+        Yrgb[1] = toLinear(color[1]);
+        Yrgb[2] = toLinear(color[2]);
+
+        return 0.9 * (Yrgb[0] * K_R + Yrgb[1] * K_G + Yrgb[2] * K_B); // 10% идет на поглощение
+    }
+
+    getYfromProps() {
+        var Props = this.Props;
+        var type = Props.Type.getValue();
+        var Refl = Props.Refl.getValue();
+        var Trans = Props.Trans.getValue();
+        var Kspec_refl = Props.Kspec_refl.getValue();
+        var Y;
+        switch (type) {
+            case "Metallic":
+                Y = Refl;
+                break;
+            case "Painted":
+                var K = Kspec_refl == 1 ? 0 : Kspec_refl * Refl;
+                Y = (Refl - K) / (1 - K);
+                // Y = Refl;
+                break;
+            case "Transparent":
+                var K = Trans / Refl;
+                Y = Refl * (1 + K);
+                //Y = Refl + Trans;
+                break;
+        }
+        return Y;
+    }
+
+    //Update ----------------------------------------------------------------
+    updateRGB() {
+        var Y = this.getYfromProps();
+        var color = changeLuminance(this.Color, Y);
+        this.setColor(color);
+    }
+
+    updateProps() {
+        var Props = this.Props;
+        var type = Props.Type.getValue();
+        var Y = this.getYfromRgb();
+        var Refl = Props.Refl.getValue();
+        var Kspec_refl = Props.Kspec_refl.getValue();
+        var Trans = Props.Trans.getValue();
+        var K = this.Koeff;
+        switch (type) {
+            case "Metallic":
+                if (K !== undefined)
+                    this.setKspec_refl(K);
+                this.setRefl(Y);
+                break;
+            case "Painted":
+                if (K === undefined)
+                    K = Kspec_refl * Refl
+
+                Refl = K + (1 - K) * Y;
+                Kspec_refl = Refl ? K / Refl : 0;
+
+                if (K != undefined && Refl > 0.9)
+                    Refl = 0.9;
+                this.setRefl(Refl);
+                this.setKspec_refl(Kspec_refl);
+                break;
+            case "Transparent":
+                if (Refl == 0) {
+                    Trans = Y;
+                } else {
+                    if (K === undefined)
+                        K = Trans / Refl;
+                    Refl = Y / (1 + K);
+                    Trans = K * Refl;
+                }
+
+                this.setRefl(Refl);
+                this.setTrans(Trans);
+                break;
+        }
+    }
+
+    update(flag = 0) {
+        if (flag == 1)
+            this.updateProps();
+        else if (flag == 2)
+            this.updateRGB();
+
+        this.render();
+    }
+
+
+
+    // SET ----------------------------------------------------------------
+    setType() {
+        var Props = this.Props;
+        var type = Props.Type.getValue();
+        if (type == "Metallic") {
+            elem_vis(Props.Kspec_refl, true);
+            elem_vis(Props.Trans, false);
+            elem_vis(Props.n, false);
+        }
+        else if (type == "Painted") {
+            elem_vis(Props.Kspec_refl, true);
+            elem_vis(Props.Trans, false);
+            elem_vis(Props.n, false);
+        }
+        else if (type == "Transparent") {
+            elem_vis(Props.Kspec_refl, false);
+            elem_vis(Props.Trans, true);
+            elem_vis(Props.n, true);
+        }
+        this.Type = type;
+        this.updateProps();
+        this.render();
+    }
+
+
+    setColor(Color) {
+        var Props = this.Props;
+        if (!check_value(Props.Color))
+            return;
+
+        if (Color !== undefined) {
+            Props.Color.upd = false;
+            Props.Color.setValue(Color);
+            return
+        }
+
+        this.Koeff = undefined;
+        this.Color = Props.Color.getValue();
+        this.update(1);
+    }
+
+    setRefl(Refl) {
+        var Props = this.Props;
+        var type = Props.Type.getValue();
+
+        if (!check_value(Props.Refl)) return;
+
+        if (Refl === undefined)
+            Refl = Props.Refl.getValue();
+        else {
+            Props.Refl.upd = false;
+            Props.Refl.setValue(Refl);
+            return
+        }
+
+        if (type == "Painted") {
+            this.Koeff = Refl * Props.Kspec_refl.getValue();
+            if (Refl == 0)
+                this.setKspec_refl(0);
+        }
+        else if (type == "Transparent") {
+            this.Koeff = 1.0;
+            if (Refl > (1 - Props.Trans.getValue())) {
+                this.setTrans(1 - Refl);
+            }
+        }
+        this.update(2);
+    }
+
+    setKspec_refl(Kspec_refl) {
+        var Props = this.Props;
+        var type = Props.Type.getValue();
+        if (!check_value(Props.Kspec_refl)) return;
+
+        if (Kspec_refl !== undefined) {
+            Props.Kspec_refl.upd = false;
+            Props.Kspec_refl.setValue(Kspec_refl);
+            //this.update(2);
+            return
+        }
+
+        Kspec_refl = Props.Kspec_refl.getValue();
+        if (type == "Metallic") {
+            this.Koeff = Kspec_refl;
+            this.update();
+        } else if (type == "Painted") {
+            this.Koeff = Kspec_refl * Props.Refl.getValue();
+            this.update(2);
+        }
+
+    }
+
+    setTrans(Trans) {
+        var Props = this.Props;
+        if (!check_value(Props.Trans)) return;
+
+        if (Trans === undefined)
+            Trans = Props.Trans.getValue();
+        else {
+            Props.Trans.upd = false;
+            Props.Trans.setValue(Trans);
+            //this.update(2);
+            return
+        }
+
+        this.Koeff = 1.0;
+
+        if (Trans > (1 - Props.Refl.getValue()))
+            this.setRefl(1 - Trans);
+
+        this.update(2);
+
+    }
+
+    setN() {
+        this.update();
+    }
+
+
+    SetMaterial(diff, spec, amb, opacity, Shin, N) {
+        this.material.color.setRGB(diff[0], diff[1], diff[2]);      // диффузная часть
+        this.material.specular.setRGB(spec[0], spec[1], spec[2]);   // зеркальный блик 
+        this.material.ambient.setRGB(amb[0], amb[1], amb[2]);       // окружение (зеркальный + прозрачный) 
+
+        this.material.opacity = opacity;           // отношение между 0 - зеркальный, 1 - прозрачный
+        this.material.Shin = Shin;                 // блеск (размер зеркального блика)
+        this.material.refractionRatio = N;     // коэфиент преломления прозрачной части
+    }
+
+
+}
+
+export { MaterialMaster }
